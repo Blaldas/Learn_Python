b3e6ea90f438c9b443d8a6d7bc4c94dc5614269f
Blaldas <marmingues00@gmail.com>
2021-09-13 01:15:53 +0100

    updated main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# Learn_Python" 
+
+Source: https://www.udemy.com/course/intro_python/learn/lecture/11785576#overview
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("olá mundo")
+var1 = 1  # integer
+var2 = 1.1  # float
+var3 = "Some text"  # String
+var4 = True  # boolean
+
+# if <condition> :
+# elif <condition> :
+# else:
+
+
+# types of lists
+list1 = [1, 3, 57, 8, 8]  # integer
+list2 = ["hello", "world", "!"]  # string
+list3 = ["word", 2.2, "hmm yes", 1, False]
+
+# iterate though a List -> for each
+for i in list1:
+    print(i)
+
+# manual loop into a list -> c style
+for i in range(len(list1)):
+    print(list1[i])
